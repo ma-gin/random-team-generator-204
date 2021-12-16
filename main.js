9f0c105dbcd7549474cccf07e08672decfbbe4df
Matt Ginter <mattginter@gmail.com>
2021-12-16 23:06:43 +0100

    Update layout

--- main.js
+++ main.js
@@ -9,17 +9,21 @@ const nameInput = document.getElementById("name-input")
 
 // ******** DECLARATION OF FUNCTIONS ********
 
-const addName = () => {
+const addNameToWaiting = () => {
+    const newName = nameInput.value
     newDiv.classList.remove("hide")
-    waitingList.push(nameInput.value)
-    listContainer.classList.remove("hide")
-    console.log(waitingList)
-
-    const listItem = document.createElement("li")
-    listItem.innerText = nameInput.value
-    namesList.appendChild(listItem)
-    // add remove button function to each item
-    nameInput.value = ""
+    waitingList.push(newName)
+    if (nameInput) {
+        listContainer.classList.remove("hide")
+        console.log(waitingList)
+        const listItem = document.createElement("li")
+        listItem.innerText = nameInput.value
+        namesList.appendChild(listItem)
+        // add remove button function to each item
+        nameInput.value = ""
+    } else {
+        console.log("works")
+    }
 }
 
 const createLists = (listLength) => {
@@ -28,22 +32,16 @@ const createLists = (listLength) => {
     const runLoop = Math.floor(listLength / teamAmount)
     console.log(runLoop)
     const newerDiv = document.createElement("div")
-    let checker = false;
+    const teamNumber = document.createElement("h4")
+    teamNumber.innerText = "Team"
+    newerDiv.appendChild(teamNumber)
     for (let i = 0; i < runLoop; i++) {
         const randomIndex = Math.floor(Math.random() * waitingList.length - 1)
         const nameToAdd = waitingList.splice(randomIndex, 1)
-        // console.log(nameToAdd[0])
-        if (checker === false) {
-            const teamNumber = document.createElement("h4")
-            teamNumber.innerText = `Team ${i + 1}`
-            newerDiv.appendChild(teamNumber)
-            checker = true
-        }
         const newItem = document.createElement("li")
         newItem.innerText = nameToAdd
         teamList.appendChild(newItem)
     } 
-    
     newerDiv.appendChild(teamList)
     teamList.classList.add("list-style")
     newDiv.appendChild(newerDiv)
@@ -60,7 +58,7 @@ const createWaitingList = () => {
 
 // How many names do we have ? DONE
 // How many teams to we want ? DONE
-// Check if it's odd.. Ask if we want even teams or not? checkbox 
+// Check if it's odd.. Ask if we want even teams or not? checkbox NOT DONE 
 // Add random name to team 1 x times, run loop x times.
 // Print team 1
 // Add random name to team 2 x times, run loop x times.
@@ -68,7 +66,6 @@ const createWaitingList = () => {
     
 const generate = () => {
     const checkEven = document.getElementById("even-option").value
-    console.log(checkEven)
     const listLength = waitingList.length
     const teamAmount = document.getElementById("number-input").value
     for (let i = 0; i < teamAmount; i++){
