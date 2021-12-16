32a6f549e0e5e6c182f25da7b0d9e287653946ff
Matt Ginter <mattginter@gmail.com>
2021-12-16 23:38:07 +0100

    Update waiting list

--- main.js
+++ main.js
@@ -10,19 +10,18 @@ const nameInput = document.getElementById("name-input")
 // ******** DECLARATION OF FUNCTIONS ********
 
 const addNameToWaiting = () => {
-    const newName = nameInput.value
     newDiv.classList.remove("hide")
-    waitingList.push(newName)
-    if (nameInput) {
+    waitingList.push(nameInput.value)
+    if (nameInput.value) {
         listContainer.classList.remove("hide")
-        console.log(waitingList)
         const listItem = document.createElement("li")
         listItem.innerText = nameInput.value
         namesList.appendChild(listItem)
         // add remove button function to each item
         nameInput.value = ""
-    } else {
-        console.log("works")
+        // } else {
+        //     console.log("working")
+        // }
     }
 }
 
@@ -30,17 +29,20 @@ const createLists = (listLength) => {
     const teamList = document.createElement("ul")
     const teamAmount = document.getElementById("number-input").value
     const runLoop = Math.floor(listLength / teamAmount)
-    console.log(runLoop)
     const newerDiv = document.createElement("div")
     const teamNumber = document.createElement("h4")
     teamNumber.innerText = "Team"
     newerDiv.appendChild(teamNumber)
     for (let i = 0; i < runLoop; i++) {
-        const randomIndex = Math.floor(Math.random() * waitingList.length - 1)
+        const randomIndex = Math.floor(Math.random() * waitingList.length)
+        console.log(randomIndex)
         const nameToAdd = waitingList.splice(randomIndex, 1)
         const newItem = document.createElement("li")
         newItem.innerText = nameToAdd
         teamList.appendChild(newItem)
+        const waitingListItems = document.querySelectorAll(".waiting-list li")
+        console.log(waitingListItems)
+        waitingListItems[randomIndex].remove()
     } 
     newerDiv.appendChild(teamList)
     teamList.classList.add("list-style")
